b3eb7de927aba2cea827b1665e2afc88d3d25173
leekalan <kalanjlee@gmail.com>
2026-07-18 16:15:05 +1000

    add global error type

---
+++ src/error.rs
@@ -0,0 +1,11 @@
+//! Crate-wide error type. Each variant wraps a specific module's error type.
+
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
